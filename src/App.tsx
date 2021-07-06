7a219f4ae54acd7a8de8c964ac093c8337734ad2
RicardoSantos-100 <ricardo.santos@docly.com.br>
2021-07-05 22:56:39 -0300

    finish day-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { ExperienceBar } from './components/ExperienceBar';
 import './styles/global.css';
 
 function App() {
   return (
-    <div className="App">
-      Testo
+    <div className="Container">
+      < ExperienceBar />
     </div>
   );
 }
