10ab3ee4c673dedca766bedc280373ec158fbdc5
RicardoSantos-100 <ricardo.santos@docly.com.br>
2021-07-09 15:00:30 -0300

    finish day 02

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img 
+        src="https://github.com/diego3g.png" 
+        alt="Ricardo"
+      />
+    
+      <div>
+        <strong>Ricardo Carvalho</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1</p>
+      </div>
+    </div>
+  )
+}
