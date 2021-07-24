101b293103532df9c5f576c04c7a7adf3d964a01
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2021-07-24 13:19:11 -0300

    finish day-03

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,19 @@
-/* eslint-disable @next/next/no-img-element */
-import styles from '../styles/components/Profile.module.css';
+import styles from '../styles/components/Profile.module.css'
 
-export function Profile() {
+export function Profile () {
   return (
     <div className={styles.profileContainer}>
-      <img 
-        src="https://github.com/diego3g.png" 
-        alt="Ricardo"
+      <img
+        src='https://github.com/diego3g.png'
+        alt='Ricardo'
       />
-    
+
       <div>
         <strong>Ricardo Carvalho</strong>
         <p>
-          <img src="icons/level.svg" alt="Level" />
-          Level 1</p>
+          <img src='icons/level.svg' alt='Level' />
+          Level 1
+        </p>
       </div>
     </div>
   )
